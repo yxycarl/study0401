4cc42cf8e0547810e03428e3127b89e446f9fc82
yxyyxy <735526652@qq.com>
2020-04-03 12:06:41 +0800

    first2提交代码

---
+++ src/com/atguigu/java/test1.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author yxycarl
+ * @date 2020/4/3 12:04
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("wo ai ni ");
+    }
+}
