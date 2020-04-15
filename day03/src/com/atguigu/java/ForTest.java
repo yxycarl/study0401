de4c11d1ad1103d466dced24c05313e44e442214
yxyyxy <735526652@qq.com>
2020-04-15 15:35:49 +0800

    first3提交代码

---
+++ day03/src/com/atguigu/java/ForTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.java;
+
+/**
+ * @author yxycarl
+ * @date 2020/4/15 15:27
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
