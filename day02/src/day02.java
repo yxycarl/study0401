e65aec2f48ef23fca2dcd9fb2d0331ed0a1a5044
yxyyxy <735526652@qq.com>
2020-04-15 14:44:03 +0800

    first3提交代码

---
+++ day02/src/day02.java
@@ -0,0 +1,26 @@
+/**
+ * @author yxycarl
+ * @date 2020/4/11 18:20
+ */
+public class day02 {
+    public static void main(String[] args) {
+       /* int number=20;
+        System.out.println(number);
+        int number2;
+        number2=50;
+        System.out.println(number2);
+        int sum;
+        sum=number+number2;
+        System.out.println(sum);
+        int n1=10,n2=20,n3=30;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);*/
+        long num1=100;
+        System.out.println(num1);
+        float num2=12.3f;
+        System.out.println(num2);
+        double num3=12.4;
+        System.out.println(num3);
+    }
+}
---
+++ day03/src/com/atguigu/java/SwitchTest.java
@@ -0,0 +1,33 @@
+package com.atguigu.java;
+import java.util.Scanner;
+
+/**
+ * @author yxycarl
+ * @date 2020/4/15 14:22
+ */
+public class SwitchTest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("年");
+        int year=s.nextInt();
+        System.out.println("月");
+        int month=s.nextInt();
+        System.out.println("日");
+        int day=s.nextInt();
+        int sumday=0;
+        switch (month){
+            case 3:
+                if(year/4==0 && year/100!=0 || year/400==0){
+                    sumday=28+sumday;
+                }else{
+                    sumday=28+sumday;
+                }
+            case 2:
+                sumday=31+sumday;
+            case 1:
+                sumday=sumday+day;
+                break;
+        }
+        System.out.println(sumday);
+    }
+}
